6c38d435f351dbe32eee6541711f5605f11bd7eb
Deniz Bozdas <dbozdas@gmail.com>
2022-06-24 13:42:21 -0500

    ccc

--- src/test/java/com/qaprosoft/carina/demo/APIMethodsTest.java
+++ src/test/java/com/qaprosoft/carina/demo/APIMethodsTest.java
@@ -5,9 +5,7 @@ import com.qaprosoft.carina.core.foundation.IAbstractTest;
 import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
 import com.qaprosoft.carina.core.foundation.utils.tag.Priority;
 import com.qaprosoft.carina.core.foundation.utils.tag.TestPriority;
-import com.qaprosoft.carina.demo.apimethods.DeletePostMethod;
-import com.qaprosoft.carina.demo.apimethods.GetPostMethod;
-import com.qaprosoft.carina.demo.apimethods.PostPostMethod;
+import com.qaprosoft.carina.demo.apimethods.*;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,5 +50,69 @@ public class APIMethodsTest implements IAbstractTest {
         postPostMethod.getProperties().remove("title");
         postPostMethod.callAPIExpectSuccess();
         postPostMethod.validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "dbozdas")
+    public void testGetAlbums() {
+        GetAlbumMethod getAlbumMethod = new GetAlbumMethod();
+        getAlbumMethod.callAPIExpectSuccess();
+        getAlbumMethod.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey());
+        getAlbumMethod.validateResponseAgainstSchema("api/albums/_get/rs.schema");
+    }
+
+    @Test()
+    @MethodOwner(owner = "dbozdas")
+    @TestPriority(Priority.P1)
+    public void testDeleteAlbums() {
+        DeleteAlbumMethod deleteAlbumMethod = new DeleteAlbumMethod();
+        deleteAlbumMethod .setProperties("api/albums/album.properties");
+        deleteAlbumMethod .callAPIExpectSuccess();
+        deleteAlbumMethod .validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "dbozdas")
+    public void testCreateAlbums() throws Exception {
+        PostAlbumMethod postAlbumMethod = new PostAlbumMethod();
+        postAlbumMethod.setProperties("api/albums/album.properties");
+        postAlbumMethod.getProperties().remove("userId");
+        postAlbumMethod.getProperties().remove("id");
+        postAlbumMethod.getProperties().remove("title");
+        postAlbumMethod.callAPIExpectSuccess();
+        postAlbumMethod.validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "dbozdas")
+    public void testGetPhotos() {
+        GetPhotoMethod getPhotoMethod = new GetPhotoMethod();
+        getPhotoMethod.callAPIExpectSuccess();
+        getPhotoMethod.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey());
+        getPhotoMethod.validateResponseAgainstSchema("api/photos/_get/rs.schema");
+    }
+
+    @Test()
+    @MethodOwner(owner = "dbozdas")
+    @TestPriority(Priority.P1)
+    public void testDeletePhotos() {
+        DeletePhotoMethod deletePhotoMethod  = new DeletePhotoMethod ();
+        deletePhotoMethod .setProperties("api/photos/photo.properties");
+        deletePhotoMethod .callAPIExpectSuccess();
+        deletePhotoMethod .validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "dbozdas")
+    public void testCreatePhotos() throws Exception {
+        PostPhotoMethod postPhotoMethod = new PostPhotoMethod();
+        postPhotoMethod .setProperties("api/photos/photo.properties");
+        postPhotoMethod .getProperties().remove("albumId");
+        postPhotoMethod .getProperties().remove("id");
+        postPhotoMethod .getProperties().remove("title");
+        postPhotoMethod .getProperties().remove("url");
+        postPhotoMethod .getProperties().remove("thumbnailUrl");
+        postPhotoMethod .callAPIExpectSuccess();
+        postPhotoMethod .validateResponse();
     }
 }
